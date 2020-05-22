2cfdb64b5dc70820db836d3ae84d8c564233114c
Alan Gu <agu@caltech.edu>
2020-05-21 21:18:39 -0700

    kohler_RT done but value not verified

--- gettsettle.py
+++ gettsettle.py
@@ -1,20 +1,38 @@
-# T[C], RH[fraction], d0[um], cNaCl[mol/L]
+# T[C], RH[%], d0[um], cNaCl[mol/L]
 
-
+import math
 
 def gettsettle_d2_epstein(T, RH, d0, cNaCl):
-	#Kohler equation
+	smallest_d = kohler_RT(RH, d0, cNaCl)
 
 	#d^2 law
 	
 	return t_settle
 
-def kohler(T, RT, cNaCl):
-	# https://en.wikipedia.org/wiki/K%C3%B6hler_theory
-	# All SI units
-	M_w = 18.02e-3
-	sigma_w = get_sigma_w(T)]
-	rho_w = 1000
+def kohler_RT(RH, d0, cSolute):
+	# Lewis 2008
+	# Kelvin effect neglible i.e big droplets
+	# return equilibrium droplet diameter in um for the given amount of salt
+
+	# empirical values for NaCl
+	a = 1.08
+	b = 1.1
+	MWdry = 58.44
+	rho_dry = 2.16 # [g/cm3]
+
+	h = min(RH/100, 0.999) # avoid h=1 case for epsilon calc
+	epsilon = a * (b + 1/(1-h))**(1/3)
+	# print('epsilon is ' + str(epsilon))
+
+	r0 = d0/2*1e-6 # [m]
+	V0 = 4/3*math.pi*r0**3*1e3 # [L]
+	ndry = cSolute*V0 # [mol]
+	mdry = ndry*MWdry # [g]
+	r_dry = (3*mdry/(4*math.pi*rho_dry))**(1/3) # [cm]
+
+	r_wet = r_dry*epsilon*1000 # [um]
+
+	return r_wet*2
 
 def get_sigma_w(TUnitC):
 	# https://srd.nist.gov/JPCRD/jpcrd231.pdf
@@ -25,6 +43,6 @@ def get_sigma_w(TUnitC):
 	B = 235.8e-3
 	b = -0.625
 	mu = 1.256
-	sigma = B*((T_c-T)/T_c)^mu*(1+b*(T_c-T)/T_c)
+	sigma = B*((T_c-T)/T_c)**mu*(1+b*(T_c-T)/T_c)
 	return sigma
 

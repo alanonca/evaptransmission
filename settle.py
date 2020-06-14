03ae4617795d9c6d188b04db7c141edb8b0f6053
Alan Gu <agu@caltech.edu>
2020-06-13 23:26:05 -0700

    add kohler algo

--- settle.py
+++ settle.py
@@ -1,19 +1,53 @@
 # T[C], RH[%], d0[um], cNaCl[mol/L]
 # assuming:
 # - droplet has constant temperature T
+# - dilute solution for kohler; any RH
 
 import math
 from scipy.interpolate import interp1d
 import numpy as np
 
-def gettsettle_d2_epstein(T, RH, d0, cNaCl):
-	# kholer size limit
-	# smallest_d = kohler_RT(RH, d0, cNaCl)
-	
+R = 8.3145
+
+def settling_time(T, RH, d0, cNaCl):
+	#Kohler size, epstein settling velocity
+	r_eq = kohler(T, RH, d0, cNaCl)
 
 
 	
 	return t_settle
+
+def kohler(T, RH, d0, cNaCl):
+	# modified eq 8 from Lewis 2008; any RH instead of just h~1
+	r_dry = r_dry_NaCl(d0, cNaCl)
+	vw_bar = partial_molal_vol(T,cNaCl)/1e6 #m3/mol
+	sigma = sigma_w(T)
+	r_sigma_o = 2*vw_bar*sigma/(R*(T+273.15)) #m
+
+	epsilon_sigma_o = r_sigma_o/r_dry
+
+	c = 1.1 #for NaCl
+
+	coeff = [math.log(RH/100), -epsilon_sigma_o, 0, c**3]
+	epsilon_roots = np.roots(coeff)
+	epsilon_root = epsilon_roots[~np.iscomplex(epsilon_roots)]
+	epsilon = epsilon_root[0].real
+
+	r_wet = r_dry*epsilon
+
+	return r_wet
+
+def r_dry_NaCl(d0, cNaCl):
+	MWdry = 58.44
+	rho_dry = 2.16 # [g/cm3]
+
+	r0 = d0/2*1e-6 # [m]
+	V0 = 4/3*math.pi*r0**3*1e3 # [L]
+	ndry = cNaCl*V0 # [mol]
+	mdry = ndry*MWdry # [g]
+	r_dry = (3*mdry/(4*math.pi*rho_dry))**(1/3) # [cm]
+
+	return r_dry/100 #m
 
 def partial_molal_vol(T, cNaCl):
 	# https://pubs.acs.org/doi/pdf/10.1021/j100697a022 eq 5 and table 2
@@ -41,29 +75,29 @@ def viability(T,RH):
 	return t_half #half life[hr]
 
 
-def d2_law(T, RH, d0, cNaCl, t):
-	# only works for >~mm size droplets
-	# T[C], RH[%], d0[um], conc[mol/L], t[s]
+# def d2_law(T, RH, d0, cNaCl, t):
+# 	# only works for >~mm size droplets
+# 	# T[C], RH[%], d0[um], conc[mol/L], t[s]
 	
-	#constants specific to NaCl
-	i = 2
-	MWNaCl = 58.44 # [g/mol]
+# 	#constants specific to NaCl
+# 	i = 2
+# 	MWNaCl = 58.44 # [g/mol]
 
-	T_K = T + 273.15
-	D = D_water_air(T)
-	M = 18.02e-3 # [kg/mol]
-	rho1 = 1000 # [kg/m3]
-	R = 8.3145 # [SI]
-	d0_m = d0*1e-6 # [m]
-	Zs = conc_to_molar_frac(cNaCl, MWNaCl)
-	P = water_vapour_pressure(T)*1e5 # [Pa]
+# 	T_K = T + 273.15
+# 	D = D_water_air(T)
+# 	M = 18.02e-3 # [kg/mol]
+# 	rho1 = 1000 # [kg/m3]
+# 	R = 8.3145 # [SI]
+# 	d0_m = d0*1e-6 # [m]
+# 	Zs = conc_to_molar_frac(cNaCl, MWNaCl)
+# 	P = water_vapour_pressure(T)*1e5 # [Pa]
 
-	# print(str(T_K)+'\n'+str(D)+'\n'+str(M)+'\n'+str(rho1)+'\n'+str(R)+'\n'+str(d0_m)+'\n'+str(Zs)+'\n'+str(P)+'\n'+str(RH))
+# 	# print(str(T_K)+'\n'+str(D)+'\n'+str(M)+'\n'+str(rho1)+'\n'+str(R)+'\n'+str(d0_m)+'\n'+str(Zs)+'\n'+str(P)+'\n'+str(RH))
 
-	LHS = 1-8*D*M/rho1/R*((P*(1-i*Zs))/T_K-P/T_K*RH/100)*t/(d0_m)**2
-	d = math.sqrt(LHS)*d0_m*1e6 # [um]
-	smallest_d = kohler_RT(RH, d0, cNaCl)
-	return max(d, smallest_d) # [um]
+# 	LHS = 1-8*D*M/rho1/R*((P*(1-i*Zs))/T_K-P/T_K*RH/100)*t/(d0_m)**2
+# 	d = math.sqrt(LHS)*d0_m*1e6 # [um]
+# 	smallest_d = kohler_RT(RH, d0, cNaCl)
+# 	return max(d, smallest_d) # [um]
 
 
 def water_vapour_pressure(T_C):
@@ -91,31 +125,31 @@ def D_water_air(T):
 	D = 22.5e-6*((T+273.15)/273.15)**1.8
 	return D # [m2/s] water vapour in air
 
-def kohler_RT(RH, d0, cSolute):
-	# RH[%], d0[um], cSolulte[mol/L]
-	# Lewis 2008
-	# Kelvin effect neglible i.e big droplets
-	# return equilibrium droplet diameter in um for the given amount of salt
+# def kohler_RT(RH, d0, cSolute):
+# 	# RH[%], d0[um], cSolulte[mol/L]
+# 	# Lewis 2008
+# 	# Kelvin effect neglible i.e big droplets
+# 	# return equilibrium droplet diameter in um for the given amount of salt
 
-	# empirical values for NaCl
-	a = 1.08
-	b = 1.1
-	MWdry = 58.44
-	rho_dry = 2.16 # [g/cm3]
+# 	# empirical values for NaCl
+# 	a = 1.08
+# 	b = 1.1
+# 	MWdry = 58.44
+# 	rho_dry = 2.16 # [g/cm3]
 
-	h = min(RH/100, 0.999) # avoid h=1 case for epsilon calc
-	epsilon = a * (b + 1/(1-h))**(1/3)
-	# print('epsilon is ' + str(epsilon))
+# 	h = min(RH/100, 0.999) # avoid h=1 case for epsilon calc
+# 	epsilon = a * (b + 1/(1-h))**(1/3)
+# 	# print('epsilon is ' + str(epsilon))
 
-	r0 = d0/2*1e-6 # [m]
-	V0 = 4/3*math.pi*r0**3*1e3 # [L]
-	ndry = cSolute*V0 # [mol]
-	mdry = ndry*MWdry # [g]
-	r_dry = (3*mdry/(4*math.pi*rho_dry))**(1/3) # [cm]
+# 	r0 = d0/2*1e-6 # [m]
+# 	V0 = 4/3*math.pi*r0**3*1e3 # [L]
+# 	ndry = cSolute*V0 # [mol]
+# 	mdry = ndry*MWdry # [g]
+# 	r_dry = (3*mdry/(4*math.pi*rho_dry))**(1/3) # [cm]
 
-	r_wet = r_dry*epsilon*1000 # [um]
+# 	r_wet = r_dry*epsilon*1000 # [um]
 
-	return r_wet*2 # [um]
+# 	return r_wet*2 # [um]
 
 def sigma_w(TUnitC):
 	# https://srd.nist.gov/JPCRD/jpcrd231.pdf
@@ -126,5 +160,5 @@ def sigma_w(TUnitC):
 	b = -0.625
 	mu = 1.256
 	sigma = B*((T_c-T)/T_c)**mu*(1+b*(T_c-T)/T_c)
-	return sigma
+	return sigma #N/m
 

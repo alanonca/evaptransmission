579b1c608fc41b448bf69065468f5075ae4fca99
Alan Gu <agu@caltech.edu>
2020-09-24 00:01:48 -0700

    add LA_Apr_to_Jul

--- runARLinReg.py
+++ runARLinReg.py
@@ -1,16 +1,16 @@
 import linreg
 import pandas as pd
 
-for csvName in ['output_Clark_arL4.csv', 'output_Harris_arL4.csv', 'output_King_arL4.csv', 
-	'output_LA_new_arL4.csv', 'output_Maricopa_arL4.csv', 'output_MiamiD_arL4.csv', 
-	'output_SantaClara_arL4.csv', 'output_SD_arL4.csv']:
+# for csvName in ['output_Clark_arL4.csv', 'output_Harris_arL4.csv', 'output_King_arL4.csv', 
+# 	'output_LA_new_arL4.csv', 'output_Maricopa_arL4.csv', 'output_MiamiD_arL4.csv', 
+# 	'output_SantaClara_arL4.csv', 'output_SD_arL4.csv']:
 
-# for csvName in ['output_LA_new_arL4.csv']:
+for csvName in ['output_LA_Apr_to_Jul_arL4.csv']:
 
 	print("\n\n")
 	print(csvName)
 	df = pd.read_csv(csvName, header=None)
 	df.columns = ['tsetL0', 'viabilityL0', 'tsetL1', 'viabilityL1', 'tsetL2', 'viabilityL2', 
 		'tsetL3', 'viabilityL3', 'tsetL4', 'viabilityL4', 'ncperc']
-	linreg.trainTestSplit(df, lastxRowForTest=4, filename=csvName)
+	linreg.trainTestSplit(df, lastxRowForTest=30, filename=csvName)
 	# linreg.regOnly(df)
--- runRNN.py
+++ runRNN.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import nntimeseries
 
-# for csvName in ['output_Clark_arL4.csv', 'output_Harris_arL4.csv', 'output_King_arL4.csv', 
-# 	'output_LA_arL4.csv', 'output_Maricopa_arL4.csv', 'output_MiamiD_arL4.csv', 
-# 	'output_SantaClara_arL4.csv', 'output_SD_arL4.csv']:
-
-for county in ['Harris', 'King', 'LA', 'Maricopa', 'MiamiD', 'SantaClara', 'SD']:
+# for county in ['Harris', 'King', 'LA', 'Maricopa', 'MiamiD', 'SantaClara', 'SD']:
+for county in ['LA_Apr_to_Jul']:
 	print(county)
 
 	arL4csv = 'output_' + county + '_arL4.csv'
-	contourcsv = 'contourdata_' + county + '_RNNin.csv'
+	# contourcsv = 'contourdata_' + county + '_RNNin.csv'
 
 	df = pd.read_csv(arL4csv, header=None)
-	dfContour = pd.read_csv(contourcsv, header=None)
+	# dfContour = pd.read_csv(contourcsv, header=None)
 	
 	df.columns = ['tsetL0', 'viabilityL0', 'tsetL1', 'viabilityL1', 'tsetL2', 'viabilityL2', 
 		'tsetL3', 'viabilityL3', 'tsetL4', 'viabilityL4', 'ncperc']
-	dfContour.columns = ['tsetL0', 'viabilityL0', 'tsetL1', 'viabilityL1', 'tsetL2', 'viabilityL2', 
-		'tsetL3', 'viabilityL3', 'tsetL4', 'viabilityL4']
+	# dfContour.columns = ['tsetL0', 'viabilityL0', 'tsetL1', 'viabilityL1', 'tsetL2', 'viabilityL2', 
+	# 	'tsetL3', 'viabilityL3', 'tsetL4', 'viabilityL4']
 
 	nntimeseries.main(df, offset=0, numInput=10, numHidden=70, maxEpochs=1e5, learnRate=1e-4, 
-		TrainTestSplit=True, lastxRowForTest=4, contourProcess = True, dfContour = dfContour)
+		TrainTestSplit=True, lastxRowForTest=30)
+	# nntimeseries.main(df, offset=0, numInput=10, numHidden=70, maxEpochs=1e5, learnRate=1e-4, 
+	# 	TrainTestSplit=True, lastxRowForTest=4, contourProcess = True, dfContour = dfContour)
